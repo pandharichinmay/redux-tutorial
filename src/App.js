621d9f49a8ff3ccef852bcfde2ec35babf96a340
Chinmay Pandhari <pandharichinmay@gmail.com>
2021-05-18 19:40:03 +0530

    Commit Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
+import Home from './components/Home';
+import HomeContainer from './containers/HomeContainer';
+import HeaderContainer from './containers/HeaderContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/*<h1>App Component</h1>*/}
+     {/*<User data={{name:'Chinmay Pandhari',age:26}}/>*/}
+     <HeaderContainer />
+     
+     <HomeContainer />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Home(props) {
+    console.warn("Props: " , props)
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://photos.zillowstatic.com/fp/26a6e69b140233d12f5522fcbec32bd7-p_h.jpg" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                        <button 
+                    onClick={
+                        ()=>{props.removeToCartHandler()}
+                        }>
+                        Remove To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
